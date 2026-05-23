075ec46ca49fbbaa46ad282f21aa67fb34823fe0
NayanDhamane21 <nayanvdhamane@gmail.com>
2026-05-24 00:23:16 +0530

    Time: 7 ms (48.19%) | Memory: 7.9 MB (38.29%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int recursion(int n){
+       if(n==0){
+          return 0;
+       }
+       if(n==1){
+          return 1;
+       }
+
+        return recursion(n-1)+recursion(n-2);
+        
+    }
+    int fib(int n) {
+        return recursion(n);
+    }
+};
